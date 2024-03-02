31d76f6629a69b6bae74a862688dff2f5db56b01
Мария Ахматгалеева <mdakhmatgaleeva@edu.hse.ru>
2024-03-02 12:56:33 +0300

    Deleted VK API part from main

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
-import requests
 from dotenv import load_dotenv
 import os
 from yandex_music import Client
 import re
 
-#-----------------------------------VK------------------------------------------
-
 load_dotenv()
 
-MY_TOKEN_VK = os.getenv('TOKEN_VK')
-MY_URL_VK = os.getenv('URL_VK')
-version = 5.199
-
-params = {
-    'access_token': MY_TOKEN_VK,
-    'fields': 'about, bdate, city, country, status, education',
-    'v': version,
-}
-
-response = requests.get(MY_URL_VK, params)
-
-df = response.json()['response'][0]
-
-#-----------------------------------Yandex Music------------------------------------------
 
 def is_latin(text):
     return bool(re.match('^[a-zA-Z]+$', text))
@@ -60,23 +42,9 @@ for track in tracks:
 #------------------------------------Writing to file-------------------------------------
 
 with open("output.txt", "w") as file:
-    file.write(
-
-        'GENERAL INFORMATION FROM VK:\n\n' +
-        'id: ' + str(df['id']) + '\n' +
-        'Name: ' + str(df[u'first_name']) + ' ' + str(df[u'last_name']) + '\n' +
-        'Status: ' + str(df['status']) + '\n' +
-        'Birthdate: ' + str(df['bdate']) + '\n' +
-        'Country, city: ' + str(df['country']['title']) + ', ' + str(df['city']['title']) + '\n' +
-        'Education: ' + str(df['university_name']) + ' (' + str(df['faculty_name']) + ')' + '\n\n\n'
-    )
-
-file.close()
-
-with open("output.txt", "a") as file1:
-    file1.write('TOP-5 MOST LIKED NOT RUSSIAN TRACKS FROM YANDEX MUSIC FROM YOUR LIKED PLAYLIST:\n\n')
+    file.write('TOP-5 MOST LIKED NOT RUSSIAN TRACKS FROM YANDEX MUSIC FROM YOUR LIKED PLAYLIST:\n\n')
 
     for track in top_tracks:
-        file1.write(f"{track['title']} - {track['artist']} ({track['likes_count']} likes)\n")
+        file.write(f"{track['title']} - {track['artist']} ({track['likes_count']} likes)\n")
 
-file1.close()
+file.close()
